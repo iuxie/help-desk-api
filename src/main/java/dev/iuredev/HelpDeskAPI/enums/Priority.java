a841778858d8d9763c7bd61761f11e0740fc218b
iuxie <iurexavier10@gmail.com>
2026-08-18 01:25:47 -0300

    feat: implements priority and status enums

---
+++ src/main/java/dev/iuredev/HelpDeskAPI/enums/Priority.java
@@ -0,0 +1,10 @@
+package dev.iuredev.HelpDeskAPI.enums;
+
+public enum Priority {
+
+    BAIXA,
+    MEDIA,
+    ALTA,
+    CRITICA
+
+}
---
+++ src/main/java/dev/iuredev/HelpDeskAPI/enums/TicketStatus.java
@@ -0,0 +1,11 @@
+package dev.iuredev.HelpDeskAPI.enums;
+
+public enum TicketStatus {
+
+    ABERTO,
+    EM_ATENDIMENTO,
+    AGUARDANDO_SOLICITANTE,
+    RESOLVIDO,
+    CANCELADO
+
+}
