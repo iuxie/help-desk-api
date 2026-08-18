8c169ca9051baa1f70e7e8fef34beb9772e9cfa0
iuxie <iurexavier10@gmail.com>
2026-08-18 01:20:35 -0300

    feat: add categories entity and migration

---
+++ src/main/java/dev/iuredev/HelpDeskAPI/category/model/CategoryModel.java
@@ -0,0 +1,47 @@
+package dev.iuredev.HelpDeskAPI.category.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(name = "sla_deadline_hours", nullable = false)
+    private Integer slaDeadlineHours;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void onCreate() {
+        createdAt = LocalDateTime.now();
+
+        if (active == null) {
+            active = true;
+        }
+    }
+
+}
