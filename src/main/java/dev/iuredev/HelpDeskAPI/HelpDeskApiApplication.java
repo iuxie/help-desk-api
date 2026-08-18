a50ac170597cdd0894b967be2d7c2615d06289eb
iuxie <iurexavier10@gmail.com>
2026-08-17 21:10:24 -0300

    chore: initial project structure

---
+++ src/main/java/dev/iuredev/HelpDeskAPI/HelpDeskApiApplication.java
@@ -0,0 +1,13 @@
+package dev.iuredev.HelpDeskAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/iuredev/HelpDeskAPI/HelpDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.iuredev.HelpDeskAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
