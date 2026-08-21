67e947ff423a51304b8d8a0bb4ed07b35bc5bcf5
iuxie <iurexavier10@gmail.com>
2026-08-21 19:43:26 -0300

    feat: add PasswordEncorderConfig class

---
+++ src/main/java/dev/iuredev/HelpDeskAPI/config/PasswordEncoderConfig.java
@@ -0,0 +1,16 @@
+package dev.iuredev.HelpDeskAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration // Configuracoes do Spring
+public class PasswordEncoderConfig {
+
+    @Bean // Registra o objeto retornando no conteiner do Spring
+    public PasswordEncoder paswordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
