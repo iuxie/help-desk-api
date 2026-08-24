1f2564eb38c7f2fb1796b292ee19476f1c4dd7b2
iuxie <iurexavier10@gmail.com>
2026-08-23 23:11:52 -0300

    feat: add findByCode method into TicketRepository

--- src/main/java/dev/iuredev/HelpDeskAPI/tickets/repository/TicketRepository.java
+++ src/main/java/dev/iuredev/HelpDeskAPI/tickets/repository/TicketRepository.java
@@ -4,6 +4,11 @@ import dev.iuredev.HelpDeskAPI.tickets.model.TicketModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TicketRepository extends JpaRepository<TicketModel, Long> {
+
+    Optional<TicketModel> findByCode(String code);
+
 }
