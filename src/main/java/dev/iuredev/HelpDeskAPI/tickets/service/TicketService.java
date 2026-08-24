76238f9309d5d08e4332cb84cee8d75d221092bc
iuxie <iurexavier10@gmail.com>
2026-08-23 23:29:41 -0300

    feat: add create ticket method in TicketService

--- src/main/java/dev/iuredev/HelpDeskAPI/tickets/service/TicketService.java
+++ src/main/java/dev/iuredev/HelpDeskAPI/tickets/service/TicketService.java
@@ -1,15 +1,23 @@
 package dev.iuredev.HelpDeskAPI.tickets.service;
 
+import dev.iuredev.HelpDeskAPI.categories.model.CategoryModel;
+import dev.iuredev.HelpDeskAPI.categories.repository.CategoryRepository;
+import dev.iuredev.HelpDeskAPI.enums.TicketStatus;
 import dev.iuredev.HelpDeskAPI.exceptions.ResourceNotFoundException;
+import dev.iuredev.HelpDeskAPI.tickets.dto.request.TicketCreateRequestDTO;
 import dev.iuredev.HelpDeskAPI.tickets.dto.response.TicketResponseDTO;
 import dev.iuredev.HelpDeskAPI.tickets.mapper.TicketMapper;
 import dev.iuredev.HelpDeskAPI.tickets.model.TicketModel;
 import dev.iuredev.HelpDeskAPI.tickets.repository.TicketRepository;
+import dev.iuredev.HelpDeskAPI.users.model.UserModel;
+import dev.iuredev.HelpDeskAPI.users.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,10 +25,14 @@ public class TicketService {
 
     private final TicketRepository repository;
     private final TicketMapper mapper;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
 
-    public TicketService(TicketRepository repository, TicketMapper mapper) {
+    public TicketService(TicketRepository repository, TicketMapper mapper, UserRepository userRepository, CategoryRepository categoryRepository) {
         this.repository = repository;
         this.mapper = mapper;
+        this.userRepository = userRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public List<TicketResponseDTO> findAllTickets() {
@@ -37,6 +49,29 @@ public class TicketService {
     public TicketResponseDTO findTicketByCode(String code) {
         Optional<TicketModel> ticket = repository.findByCode(code);
         return mapper.toDTO(ticket.orElseThrow(() -> new ResourceNotFoundException("Chamado não encontrado.")));
+    }
+
+    @Transactional
+    public TicketResponseDTO createTicket(TicketCreateRequestDTO requestDTO) {
+        Optional<UserModel> userModel = userRepository.findById(requestDTO.requesterId());
+        Optional<CategoryModel> categoryModel = categoryRepository.findById(requestDTO.categoryId());
+        if (userModel.isPresent()) {
+            if (categoryModel.isPresent()) {
+                TicketModel ticketModel = mapper.toEntity(requestDTO);
+                ticketModel.setCode(generateCode());
+                ticketModel.setStatus(TicketStatus.ABERTO);
+                TicketModel savedModel = repository.save(ticketModel);
+                return mapper.toDTO(savedModel);
+            }
+            throw new ResourceNotFoundException("Categoria não encontrada.");
+        }
+        throw new ResourceNotFoundException("Usuário não encontrado.");
+    }
+
+    private String generateCode() {
+        return "TKT-" + UUID.randomUUID()
+                .toString()
+                .toUpperCase(Locale.ROOT);
     }
 
 }
