6c300bb2d6c440a9bc9450b7de1310157e4d6ec5
iuxie <iurexavier10@gmail.com>
2026-08-25 19:42:02 -0300

    fix: fixes comment update request DTO attributes

--- src/main/java/dev/iuredev/HelpDeskAPI/comments/dto/request/CommentUpdateRequestDTO.java
+++ src/main/java/dev/iuredev/HelpDeskAPI/comments/dto/request/CommentUpdateRequestDTO.java
@@ -1,11 +1,9 @@
 package dev.iuredev.HelpDeskAPI.comments.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CommentUpdateRequestDTO(
-        @NotBlank @Size(max = 1000) String message,
-        @NotNull Boolean internal
+        @NotBlank @Size(max = 1000) String message
 ) {
 }
