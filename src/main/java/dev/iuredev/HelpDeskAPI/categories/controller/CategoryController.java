ff1318420cab72079729bc600ae71b48978b4c90
iuxie <iurexavier10@gmail.com>
2026-08-27 22:16:11 -0300

    fix: remove try catch from CtageoryController

--- src/main/java/dev/iuredev/HelpDeskAPI/categories/controller/CategoryController.java
+++ src/main/java/dev/iuredev/HelpDeskAPI/categories/controller/CategoryController.java
@@ -4,8 +4,6 @@ import dev.iuredev.HelpDeskAPI.categories.dto.request.CategoryChangeStatusReques
 import dev.iuredev.HelpDeskAPI.categories.dto.request.CategoryRequestDTO;
 import dev.iuredev.HelpDeskAPI.categories.dto.response.CategoryResponseDTO;
 import dev.iuredev.HelpDeskAPI.categories.service.CategoryService;
-import dev.iuredev.HelpDeskAPI.exceptions.BusinessException;
-import dev.iuredev.HelpDeskAPI.exceptions.ResourceNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,56 +34,26 @@ public class CategoryController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findCategoryById(@PathVariable Long id) {
-        try {
-            CategoryResponseDTO responseDTO = service.findCategoryById(id);
-            return ResponseEntity.ok(responseDTO);
-        } catch (ResourceNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(e.getMessage());
-        }
+            return ResponseEntity.ok(service.findCategoryById(id));
     }
 
     @PostMapping
-    public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryRequestDTO requestDTO) {
-        try {
-            CategoryResponseDTO responseDTO = service.createCategory(requestDTO);
+    public ResponseEntity<CategoryResponseDTO> createCategory(@Valid @RequestBody CategoryRequestDTO requestDTO) {
             return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(responseDTO);
-        } catch (BusinessException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(e.getMessage());
-        }
+                    .body(service.createCategory(requestDTO));
     }
 
     @PatchMapping("/{id}/status")
     public ResponseEntity<?> changeCategoryStatus(
             @PathVariable Long id,
             @Valid @RequestBody CategoryChangeStatusRequestDTO requestDTO) {
-        try {
-            CategoryResponseDTO responseDTO = service.changeCategoryStatus(id, requestDTO);
-            return ResponseEntity.ok(responseDTO);
-        } catch (ResourceNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(e.getMessage());
-        } catch (BusinessException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(e.getMessage());
-        }
+            return ResponseEntity.ok(service.changeCategoryStatus(id, requestDTO));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateCategory(@PathVariable Long id,
                                             @Valid @RequestBody CategoryRequestDTO requestDTO) {
-        try {
-            CategoryResponseDTO responseDTO = service.updateCategory(id, requestDTO);
-            return ResponseEntity.ok(responseDTO);
-        } catch (ResourceNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(e.getMessage());
-        } catch (BusinessException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(e.getMessage());
-        }
+            return ResponseEntity.ok(service.updateCategory(id, requestDTO));
     }
 
 }
